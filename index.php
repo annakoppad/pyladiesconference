b330e238572038de5110d340548a9837bc8d0a4f
Annapoornima Koppad <annakoppad@gmail.com>
2016-11-18 07:25:31 +0530

    changed index file

--- index.php
+++ index.php
@@ -2,16 +2,30 @@
     <title> PyLadies Conference Website
     </title>
     <body>
-         <h1>Welcome to the Pyladies Conference Website!!!!</h1>
+         <h1 align ="center">Welcome to the Pyladies Conference Website!!!!</h1>
          <table width="10%">
          	<tr>
          		<td>Home</td>
+         	</tr>
+         	<tr>
          		<td>Speakers</td>
+        	</tr>
+        	<tr>
          		<td>Schedule</td>
+         	</tr>
+         	<tr>
          		<td>Venue</td>
+         	</tr>
+         	<tr>
          		<td>Sponsors</td>
+         	</tr>
+         	<tr>
          		<td>Sponsorship</td>
+         	</tr>
+         	<tr>
          		<td>FAQ</td>
+         	</tr>
+         	<tr>
          		<td>COC</td>
          	</tr>
          </table>
