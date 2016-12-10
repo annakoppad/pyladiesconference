13a8083434ea764975d6bb00102a86e6f6b9ff5e
Annapoornima Koppad <annakoppad@gmail.com>
2016-12-10 20:18:35 +0530

    changed size of heading

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     <title> PyLadies Conference Website
     </title>
     <body>
-         <h1 align ="center">Welcome to the Pyladies Conference Website!!!!</h1>
+         <h2 align ="center">Welcome to the Pyladies Conference Website!!!!</h2>
          <div style="float: left">
          <table>
          	<tr>
